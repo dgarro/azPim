a3c1914867a2ea75a0c98428732ee2e1f60c484a
Dan Garro <dsgarro@gmail.com>
2023-02-21 23:47:47 -0500

    Adding group support

--- src/background.js
+++ src/background.js
@@ -1,24 +1,64 @@
 chrome.webRequest.onSendHeaders.addListener(
-    function (info) {
-      if (info.requestHeaders) {
-        for(var i = 0; i < info.requestHeaders.length; i++) {
-          if(info.requestHeaders[i].name?.toLocaleLowerCase() == 'authorization') {
-            let data = { 
-              'tokenObj': { 
-                'token': info.requestHeaders[i].value, 
-                'date': new Date().toLocaleTimeString()
-              }
-            };
-            chrome.storage.session.set(data);
-            break;
-          }
-        }      
+  function (info) {
+    if (info.requestHeaders) {
+      for (var i = 0; i < info.requestHeaders.length; i++) {
+        if (info.requestHeaders[i].name?.toLocaleLowerCase() == 'authorization') {
+          let data = {
+            'tokenObj': {
+              'token': info.requestHeaders[i].value,
+              'date': new Date().toLocaleTimeString()
+            }
+          };
+          chrome.storage.session.set(data);
+          break;
+        }
       }
-    },
-    // filters
-    {
-      urls: [        
-        "https://management.azure.com/*"
-      ]
-    },
-    ["requestHeaders"]);
+    }
+  },
+  // filters
+  {
+    urls: [
+      "https://management.azure.com/*",
+    ]
+  },
+  ["requestHeaders"]);
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (info) {
+    if (info.requestBody) {
+      const body = parseRequestBody(info.requestBody);
+      let data = {
+        'altData': {
+          'altPortalAuthorization': body.altPortalAuthorization,
+          'portalAuthorization': body.portalAuthorization,
+          'tenant': body.tenant
+        }
+      };
+      chrome.storage.session.set(data);
+    }
+  },
+  // filters
+  {
+    urls: [
+      "https://portal.azure.com/api/DelegationToken",
+    ]
+  },
+  ["requestBody"]);
+
+
+function parseRequestBody(requestBody) {
+  if (!requestBody.raw) {
+    return null;
+  }
+
+  try {
+    const stringBody = String.fromCharCode.apply(
+      null,
+      new Uint8Array(requestBody.raw[0].bytes)
+    );
+    return JSON.parse(stringBody);
+  } catch (error) {
+    return null;
+  }
+}
--- src/popup.js
+++ src/popup.js
@@ -1,123 +1,247 @@
 
-    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
 
-        var parts = ResourceBuilder.build(tab.url);
-        
-        if (ResourceBuilder.valid(parts)) {
-            registerAction("#resource-info", parts?.resource);
-            registerAction("#group-info", parts?.group);
+    var parts = ResourceBuilder.build(tab.url);
 
-            // Test that we have a token
-            chrome.storage.session.get('tokenObj', function (res) {
-                if(res.tokenObj?.token == null) {
-                    StateManagement.setOverlayMessage("Temporarily unavailable - Please refresh the Azure page.");
-                } else {
-                    StateManagement.setStateAvailable();
-                }                
-            });            
-        } else {
-            StateManagement.setStateUnavailable(tab.url);
-        }
+    if (ResourceBuilder.valid(parts)) {
+        registerAction("#resource-info", parts?.resource, parts);
+        registerAction("#group-info", parts?.group, parts);
+
+        // Test that we have a token
+        chrome.storage.session.get('tokenObj', function (res) {
+            if (res.tokenObj?.token == null) {
+                StateManagement.setOverlayMessage("Temporarily unavailable - Please refresh the Azure page.");
+            } else {
+                StateManagement.setStateAvailable();
+                StateManagement.setLoading(true);
+                loadGroupResources(parts, evaluateGroups, () => errorCallback(container));
+
+            }
+        });
+    } else {
+        StateManagement.setStateUnavailable(tab.url);
+    }
+});
+
+function evaluateGroups(resp) {
+    console.log("Response => ", resp);
+
+    const root = document.querySelector("#pim-container");
+    const template = document.getElementById("group-container");
+
+    resp.forEach(element => {
+        console.log(element);
+        const firstClone = template.content.cloneNode(true);
+        const target = firstClone.querySelector(".target");
+        target.textContent = element.displayName;
+        const url = `https://portal.azure.com/#view/Microsoft_Azure_PIMCommon/ResourceMenuBlade/~/MyActions/resourceId/${element.objectId}/resourceType/Security/provider/aadGroup/resourceDisplayName/${encodeURIComponent(element.displayName)}/resourceExternalId/${element.objectId}`;
+        target.dataset.url = url;
+        const type = firstClone.querySelector(".type");
+        type.textContext = element.objectType;
+        root.appendChild(firstClone);
     });
 
-    function evaluateResources(resourceInfo, resp) {        
-        var data = resp.value;
-        if (data.length === 0) {
-            // If we don't return any items, goto the PIM page
+    document.querySelectorAll(".dynamic-group").forEach((x) => {
+        x.addEventListener('click', (event) => {
+            createNewTab(event.target.dataset.url);
+        });
+    });
+
+    StateManagement.setLoading(false);
+}
+
+function evaluateResources(resourceInfo, resp) {
+    var data = resp.value;
+    if (data.length === 0) {
+        // If we don't return any items, goto the PIM page
+        navigateToGenericPim();
+    } else if (data.length === 1) {
+        // If we return a single item = open up the PIM page
+        navigateToResourcePim(data[0]);
+    } else {
+        // ELSE we have multiple items
+        let target = null;
+        for (const v in data) {
+            // Find the item where the name and type matches
+            if (data[v].displayName == resourceInfo.name && data[v].type == resourceInfo.subtype) {
+                target = data[v];
+                break;
+            }
+        }
+        if (target === null) {
+            // If we don't find an item, goto the PIM page
             navigateToGenericPim();
-        } else if (data.length === 1) {
-            // If we return a single item = open up the PIM page
-            navigateToResourcePim(data[0]);
         } else {
-            // ELSE we have multiple items
-            let target = null;
-            for (const v in data) {
-                // Find the item where the name and type matches
-                if (data[v].displayName == resourceInfo.name && data[v].type == resourceInfo.subtype) {
-                    target = data[v];
-                    break;
-                }
-            }
-            if (target === null) {
-                // If we don't find an item, goto the PIM page
-                navigateToGenericPim();
-            } else {
-                // If we find an item, goto the PIM page
-                navigateToResourcePim(target);
-            }
+            // If we find an item, goto the PIM page
+            navigateToResourcePim(target);
         }
     }
+}
 
-    function errorCallback(container) {
-        document.querySelector(container).classList.add("error");
-        StateManagement.setLoading(false);
+function errorCallback(container) {
+    document.querySelector(container).classList.add("error");
+    StateManagement.setLoading(false);
+}
+
+function registerAction(container, data, fullData) {
+    if (data) {
+        document.querySelector(container).querySelector(".target").innerHTML = data.name;
+
+        var clicker = document.querySelector(container);
+        clicker.addEventListener("click", () => {
+            StateManagement.setLoading(true);
+            loadPimResources(data, evaluateResources, () => errorCallback(container));
+        }, false);
+
+    } else {
+        StateManagement.setVisible(container, false);
+    }
+}
+
+function openAzure() {
+    createNewTab("https://portal.azure.com");
+}
+
+function createNewTab(url) {
+    chrome.tabs.create({ active: true, url: url });
+}
+
+function navigateToGenericPim() {
+    createNewTab("https://portal.azure.com/#view/Microsoft_Azure_PimCommon/CommonMenuBlade/~/quickStart");
+}
+
+function navigateToResourcePim(data) {
+    const url = `https://portal.azure.com/#view/Microsoft_Azure_PIMCommon/ResourceMenuBlade/~/MyActions/resourceId/${data.id}/resourceType/${encodeURIComponent(data.type)}/provider/azurerbac`;
+    createNewTab(url);
+}
+
+// Adheres to RFC 3986
+function fixedEncodeURIComponent(str) {
+    return encodeURIComponent(str).replace(
+        /[!'()*]/g,
+        (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`,
+    );
+}
+
+function buildURL(resourceInfo) {
+    var type = "";
+    if (resourceInfo.type == 'resourcegroup') {
+        type = "type eq 'resourcegroup'";
+    } else {
+        type = "type ne 'resourcegroup' and type ne 'managementgroup' and type ne 'subscription'";
     }
 
-    function registerAction(container, data) {
-        if (data) {
-            document.querySelector(container).querySelector(".target").innerHTML = data.name;
+    let filter = `(${type}) and (originTenantId ne '00000000-0000-0000-0000-00000000000') and (contains(tolower(displayName),'${resourceInfo.name.toLocaleLowerCase()}'))`;
+    let encodedFilter = fixedEncodeURIComponent(filter);
 
-            var clicker = document.querySelector(container);
-            clicker.addEventListener("click", () => {
-                StateManagement.setLoading(true);
-                loadPimResources(data, evaluateResources, () => errorCallback(container));
-            }, false);
+    let url = `https://api.azrbac.mspim.azure.com/api/v2/privilegedAccess/azureResources/resources?$select=id,displayName,type,externalId&$expand=parent&$filter=(${encodedFilter})&$top=10`;
 
-        } else {
-            StateManagement.setVisible(container, false);
-        }
-    }
+    return url;
+}
 
-    function openAzure() {
-        createNewTab("https://portal.azure.com");
-    }
+function loadGraphDelegationToken(tokenData) {
+    return fetch('https://portal.azure.com/api/DelegationToken', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "authority": "portal.azure.com",
+            "method": "POST",
+            "path": "/api/DelegationToken",
+            "scheme": "https",
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en",
+            "origin": "https://portal.azure.com",
+            "referer": "https://portal.azure.com/",
+            "x-requested-with": "XMLHttpRequest"
+        },
+        body: JSON.stringify({
+            'extensionName': 'Microsoft_Azure_AD',
+            'resourceName': 'graph',
+            'tenant': `${tokenData.tenant}`,
+            'portalAuthorization': `${tokenData.portalAuthorization}`,
+            'altPortalAuthorization': `${tokenData.altPortalAuthorization}`
+        })
+    })
+        .then(response => response.json());
+}
 
-    function createNewTab(url) {
-        chrome.tabs.create({ active: true, url: url });
-    }
+function loadRoleAssignments(data, token) {
+    var url = `https://management.azure.com/subscriptions/${data.subscription.name}/resourceGroups/${data.group.name}/providers/${encodeURIComponent(data.resource.subtype)}/${data.resource.name}/providers/Microsoft.Authorization/roleAssignments?$filter=atScope()&api-version=2020-04-01-preview`;
+    return fetch(url, {
+        headers: { Authorization: token, 'Access-Control-Allow-Origin': '*' }
+    })
+        .then(resp => resp.json());
+}
 
-    function navigateToGenericPim() {
-        createNewTab("https://portal.azure.com/#view/Microsoft_Azure_PimCommon/CommonMenuBlade/~/quickStart");
-    }
+function loadObjectsByIds(tenant, altToken, ids) {
+    return fetch(`https://graph.windows.net/${tenant}/getObjectsByObjectIds`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: altToken,
+            "api-version": "1.61-internal"
+        },
+        body: JSON.stringify({
+            includeDirectoryObjectReferences: true,
+            objectIds: ids
+        })
+    })
+        .then(response => response.json());
+}
 
-    function navigateToResourcePim(data) {
-        const url = `https://portal.azure.com/#view/Microsoft_Azure_PIMCommon/ResourceMenuBlade/~/MyActions/resourceId/${data.id}/resourceType/${encodeURIComponent(data.type)}/provider/azurerbac`;
-        createNewTab(url);
-    }
+function loadGroupResources(data, successCallback, errorCallback) {
+    chrome.storage.session.get('altData', function (res) {
+        console.log("DATA => ", res);
 
-    // Adheres to RFC 3986
-    function fixedEncodeURIComponent(str) {
-        return encodeURIComponent(str).replace(
-            /[!'()*]/g,
-            (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`,
-        );
-    }
+        loadGraphDelegationToken(res.altData)
+            .then(response => {
+                console.log("loadGraphDelegationToken => ", response);
 
-    function buildURL(resourceInfo) {
-        var type = "";
-        if (resourceInfo.type == 'resourcegroup') {
-            type = "type eq 'resourcegroup'";
-        } else {
-            type = "type ne 'resourcegroup' and type ne 'managementgroup' and type ne 'subscription'";
-        }
-    
-        let filter = `(${type}) and (originTenantId ne '00000000-0000-0000-0000-00000000000') and (contains(tolower(displayName),'${resourceInfo.name.toLocaleLowerCase()}'))`;
-        let encodedFilter = fixedEncodeURIComponent(filter);
+                const tenant = res.altData.tenant;
+                const altToken = response.value.authHeader;
 
-        let url = `https://api.azrbac.mspim.azure.com/api/v2/privilegedAccess/azureResources/resources?$select=id,displayName,type,externalId&$expand=parent&$filter=(${encodedFilter})&$top=10`;
+                chrome.storage.session.get('tokenObj', function (res) {                   
+                    var token = res.tokenObj.token;
 
-        return url;
-    }
+                    loadRoleAssignments(data, token)
+                        .then(resp => {
+                            console.log("loadRoleAssignments => ", resp);
+                            if (resp.error == undefined) {
+                                console.log("GOT some roles => ", resp);
+                                const ids = resp.value.map(x => x.properties.principalId);
 
-    function loadPimResources(resourceInfo, successCallback, errorCallback) {
+                                loadObjectsByIds(tenant, altToken, ids)
+                                    .then(response => {
+                                        const results = response.value.map(x => x.displayName);
+                                        console.log(results);
+                                        successCallback(response.value);
+                                    });
+                            } else {
+                                console.error("Got an error => ", resp);
+                                errorCallback();
+                            }
+                        }, err => {
+                            console.error("Got an error => ", err);
+                            errorCallback();
+                        });
+                });
+            }, err => {
+                console.error("Got an error => ", err);
+                errorCallback();
+            });
+    });
+}
 
-        chrome.storage.session.get('tokenObj', function (res) {
-            var url = buildURL(resourceInfo);
-            var token = res.tokenObj.token;
+function loadPimResources(resourceInfo, successCallback, errorCallback) {
 
-            fetch(url, {
-                headers: { Authorization: token }
-            })
+    chrome.storage.session.get('tokenObj', function (res) {
+        var url = buildURL(resourceInfo);
+        var token = res.tokenObj.token;
+
+        fetch(url, {
+            headers: { Authorization: token }
+        })
             .then(resp => resp.json())
             .then(resp => {
                 if (resp.error == undefined) {
@@ -131,5 +255,5 @@
                 console.error("Got an error => ", err);
                 errorCallback();
             });
-        });
-    }
+    });
+}
